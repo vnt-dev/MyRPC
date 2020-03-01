21b02f5f2d418c6c1b5ea096e65516f2b1f19760
bl179 <lubl1997@qq.com>
2020-03-01 11:53:58 +0800

    测试类注释

--- src/test/java/com/wherewego/rpc/RpcApplicationTests.java
+++ src/test/java/com/wherewego/rpc/RpcApplicationTests.java
@@ -8,11 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RpcApplicationTests {
 
+    //客户端使用此注解创建实例，将调用服务端对应实现
     @Reference
     private ITest test;
     @Test
     void contextLoads() {
+        //查找服务器端对应方法并执行
         System.out.println("test"+test.test("hello world"));
+       // System.out.println("test"+test.toString());toString方法也需要实现，不能直接调用
 //        if(test!=null){
 //            test.test();
 //            System.out.println(test.test());
